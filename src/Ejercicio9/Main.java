2e768418349b883b0200b078d1e478fe193111a9
Pablo David Gonzalez Peñate <p.gonzalez@geniuskg.local>
2026-04-17 21:00:26 -0600

    Ejercicio9

---
+++ src/Ejercicio9/Main.java
@@ -0,0 +1,8 @@
+package Ejercicio9;
+
+public class Main {
+	public static void main(String[] args) {
+        LibroDigital l = new LibroDigital("El sembrador", "pabloG", 5.5);
+        l.mostrarInfo();
+    }
+}
